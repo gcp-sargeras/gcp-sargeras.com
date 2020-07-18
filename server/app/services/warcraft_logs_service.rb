7fad92f278baa4f138e713b4e9793d86ccf3dc41
Jacob Feldman <me@jacobfeldman.dev>
2020-07-18 17:38:31 -0400

    Builds out base of api requests

--- server/app/services/warcraft_logs_service.rb
+++ server/app/services/warcraft_logs_service.rb
@@ -1,3 +1,20 @@
 class WarcraftLogsService
+  def initialize
+    @client = JSONClient.new(
+      base_url: 'https://www.warcraftlogs.com/v1/',
+      default_headers: [{ 'Accept': 'application/json' }]
+    )
 
+    @required_args = [{ query: { api_key: ENV['WARCRAFT_LOGS_KEY'] } }]
+  end
+
+  def get(path, *args, &block)
+    @client.get(path, *args, *@required_args, &block)
+  end
+
+  def post(path, *args, &block)
+    @client.post(path, *args, *@required_args, &block)
+  end
 end
+
+# test.get('report/fights/2yAGX4kLnZmBx1Tt', query: {api_key: '1eba4e395450959d14254621589becfc'})
