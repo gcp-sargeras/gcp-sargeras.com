05aadcd1e5eaa4d3583891386893a1f03372d917
Jacob Feldman <me@jacobfeldman.dev>
2020-07-18 15:38:33 -0400

    Schema update for last commit

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_033929) do
+ActiveRecord::Schema.define(version: 2020_07_18_192027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,13 @@ ActiveRecord::Schema.define(version: 2020_02_13_033929) do
     t.string "discord_id"
   end
 
-  create_table "warcraftlogs", force: :cascade do |t|
-    t.string "url"
-    t.bigint "user_id"
+  create_table "warcraft_logs_characters", force: :cascade do |t|
+    t.string "name"
+    t.string "klass"
+    t.string "sub_klass"
+    t.string "server"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_warcraftlogs_on_user_id"
   end
 
 end
