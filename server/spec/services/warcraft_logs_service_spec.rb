810e40107a01f5e5c42bc8683d6342011451825e
Jacob Feldman <me@jacobfeldman.dev>
2020-07-22 18:57:27 -0400

    Adds two specs

--- server/spec/services/warcraft_logs_service_spec.rb
+++ server/spec/services/warcraft_logs_service_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WarcraftLogsService do
-  describe "#get_guild(name, server, region)"
+  describe '#get_guild(name, server, region)' do
+    it 'Gets a guilds information' do
+      expect(described_class.new.get_guild('Grand Central Parkway', 'Sargeras', 'US').status).to eq(200)
+    end
+  end
+
+  describe '#load_gcp_sargeras' do
+    it 'Loads Grand Central Parkway into database' do
+      ids = described_class.new.load_gcp_sargeras
+
+      expect(WarcraftLogs::Report.where(id: ids.rows[0][0])).to exist
+    end
+  end
 end
