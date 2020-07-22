50fabca4a580bce88a4d6d907677929b8b506231
Jacob Feldman <me@jacobfeldman.dev>
2020-07-22 18:03:07 -0400

    Creates a method to autoload gcp fights

--- server/app/services/warcraft_logs_service.rb
+++ server/app/services/warcraft_logs_service.rb
@@ -8,7 +8,14 @@ class WarcraftLogsService
     @required_args = [{ query: { api_key: ENV['WARCRAFT_LOGS_KEY'] } }]
   end
 
-  def load_reports
+  def load_gcp_sargeras
+    fights = get_guild('Grand Central Parkway', 'Sargeras', 'US').body
+
+    WarcraftLogs::Report.insert_all(fights)
+  end
+
+  def get_guild(name, server, region)
+    get("reports/guild/#{ERB::Util.url_encode(name)}/#{server}/#{region}")
   end
 
   protected
