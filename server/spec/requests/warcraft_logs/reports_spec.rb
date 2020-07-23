2c3dfcdb1c6af5b429497bc4ba2fb9b17d00a4a6
Jacob Feldman <me@jacobfeldman.dev>
2020-07-22 21:05:46 -0400

    Creates report spec

---
+++ server/spec/requests/warcraft_logs/reports_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Warcraft Logs' do
+  describe 'Reports' do
+    it 'gets and index of reports' do
+      get '/warcraft_logs/reports'
+
+      expect(response).to be_successful
+    end
+  end
+end
