ebb1ab66e2290810e8af82433f542ff182137c18
Jacob Feldman <me@jacobfeldman.dev>
2020-07-22 21:45:36 -0400

    Adds type

--- server/spec/requests/warcraft_logs/reports_request_spec.rb
+++ server/spec/requests/warcraft_logs/reports_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Warcraft Logs' do
+describe 'Warcraft Logs', type: :request do
   describe 'Reports' do
     it 'gets and index of reports' do
       get '/warcraft_logs/reports'
