8bed832e2284a73ff4b194f5751af7a195d76b27
Jacob Feldman <me@jacobfeldman.dev>
2020-07-22 21:05:27 -0400

    Limits reports routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :warcraft_logs do
-    resources :reports
+    resources :reports, only: [:index]
   end
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
