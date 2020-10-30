7b8678f3c20d586661f943e77d9ed7e384f71799
Jacob Feldman <me@jacobfeldman.dev>
2020-10-30 14:46:25 -0400

    Adds index to migration

--- server/db/migrate/20200723014122_create_warcraft_logs_fights.rb
+++ server/db/migrate/20200723014122_create_warcraft_logs_fights.rb
@@ -1,7 +1,7 @@
 class CreateWarcraftLogsFights < ActiveRecord::Migration[6.0]
   def change
     create_table :warcraft_logs_fights do |t|
-      t.references :warcraft_logs_reports
+      t.references :warcraft_logs_reports, index: true
 
       t.timestamp :start_time
       t.timestamp :end_time
