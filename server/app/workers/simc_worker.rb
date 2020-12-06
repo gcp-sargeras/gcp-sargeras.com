0202f03d7c96dfd5a46bf804bb8df2d7a0f67604
Jacob Feldman <me@jacobfeldman.dev>
2020-12-05 20:26:28 -0500

    Adds more information on output

--- server/app/workers/simc_worker.rb
+++ server/app/workers/simc_worker.rb
@@ -3,6 +3,7 @@ class SimcWorker
   sidekiq_options retry: false
 
   def perform(report_id)
+    start_time = Time.now
     report = Simc::Report.find(report_id)
     html_file_location = "#{Rails.root.join('reports')}/#{report.id}.html"
     json_file_location = "#{Rails.root.join('reports')}/#{report.id}.json"
@@ -14,9 +15,10 @@ class SimcWorker
 
     Discordrb::API::Channel.delete_message("Bot #{ENV['DISCORD_TOKEN']}", ENV['DISCORD_CHANNEL'], report.message_id) if report.message_id
 
+    end_time = Time.now
     if status == 0
       report.update(html_report: File.read(html_file_location),
-                    json_report: File.read(json_file_location))
+                    json_report: JSON.parse(File.read(json_file_location)))
       File.delete(html_file_location)
       File.delete(json_file_location)
 
@@ -24,8 +26,11 @@ class SimcWorker
       Discordrb::Bot.new(token: ENV['DISCORD_TOKEN']).send_message(
         ENV['DISCORD_CHANNEL'],
         <<~MESSAGE
-          Done simulating #{report.server}/#{report.character}
+          __Character: #{report.server}/#{report.character}__
+          **DPS**: #{report.json_report["sim"]["statistics"]["raid_dps"]["mean"].to_i}
           View report at: #{ENV['APP_URL']}/simc/reports/#{report.id}
+
+          Total Simulation Time: #{(end_time - start_time).round(2)}s
         MESSAGE
         .tap { |string| string << "\nDEBUG: #{report.as_json(except: [:json_report, :html_report]).to_s}" if Rails.env.development? }
       )
