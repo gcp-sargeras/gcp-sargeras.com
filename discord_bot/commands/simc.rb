1dabcdacabf64f3fbad81a81a30e57f94b588cb0
Jacob Feldman <me@jacobfeldman.dev>
2020-12-12 17:19:17 -0500

    Refactors out report and request into methods

--- discord_bot/commands/simc.rb
+++ discord_bot/commands/simc.rb
@@ -10,14 +10,26 @@ module DiscordBot
       def subscribe
         @bot.command :sim do |event, *args|
           resp = event.respond("Starting sim for #{args.first}")
+
           RestClient.post(
             "#{ENV['APP_URL']}/simc/reports",
-            { report: { character: args.first, message_id: resp.id } },
+            { report: report(args.first, resp.id),
+              requester: requester(event) },
             { Authorization: "Bearer #{ENV['APP_TOKEN']}" }
           )
 
           nil
         end
+      end
+
+      protected
+
+      def report(character, message_id)
+        { character: character, message_id: message_id }
+      end
+
+      def requester(event)
+        { user: { id: event.user.id }, message_id: event.message.id }
       end
     end
   end
