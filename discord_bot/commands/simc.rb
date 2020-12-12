fff1dad45d576a979575f731ac922fb3efcb1534
Jacob Feldman <me@jacobfeldman.dev>
2020-12-12 17:27:54 -0500

    Adds an error message if post request fails

--- discord_bot/commands/simc.rb
+++ discord_bot/commands/simc.rb
@@ -11,18 +11,30 @@ module DiscordBot
         @bot.command :sim do |event, *args|
           resp = event.respond("Starting sim for #{args.first}")
 
-          RestClient.post(
-            "#{ENV['APP_URL']}/simc/reports",
-            { report: report(args.first, resp.id),
-              requester: requester(event) },
-            { Authorization: "Bearer #{ENV['APP_TOKEN']}" }
-          )
+          catch_error(event, resp) do
+            RestClient.post(
+              "#{ENV['APP_URL']}/simc/reports",
+              { report: report(args.first, resp.id),
+                requester: requester(event) },
+              { Authorization: "Bearer #{ENV['APP_TOKEN']}" }
+            )
+          end
 
           nil
         end
       end
 
       protected
+
+      def catch_error(event, resp)
+        yield
+      rescue => e
+        resp.delete
+        event.respond(
+          "#{event.user.mention} An error has occurred while trying to place you in queue, please try again later."
+        )
+        raise e
+      end
 
       def report(character, message_id)
         { character: character, message_id: message_id }
