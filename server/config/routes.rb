910fc3973dd3b1dd4eb2c3efcd7640969f414b21
Jacob Feldman <me@jacobfeldman.dev>
2020-12-13 01:31:11 -0500

    Only mounts sidekiq in dev

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 end
