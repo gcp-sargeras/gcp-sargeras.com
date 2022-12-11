3b85dd296138df8d2197aac5d4d2dd7588dc91aa
Jacob Feldman <me@jacobfeldman.dev>
2022-12-11 04:26:31 -0500

    Updates various things

--- server/config/application.rb
+++ server/config/application.rb
@@ -33,5 +33,10 @@ module Server
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    # Enable sessions
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
