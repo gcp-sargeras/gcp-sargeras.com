e030e5753362d91410bce7f1ea25b513b15392da
Jacob Feldman <me@jacobfeldman.dev>
2022-12-12 00:41:48 -0500

    Updates server to 3.1.3 and rails 7

--- server/config/application.rb
+++ server/config/application.rb
@@ -15,6 +15,7 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 require 'jsonclient'
+require 'open3'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_041650) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_11_234829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_12_13_041650) do
     t.string "region", default: "us", null: false
     t.text "options"
     t.text "html_report"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "message_id"
     t.jsonb "json_report", default: "{}", null: false
     t.bigint "requester_id"
@@ -35,15 +34,15 @@ ActiveRecord::Schema.define(version: 2020_12_13_041650) do
   create_table "tokens", force: :cascade do |t|
     t.string "app"
     t.string "token"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "discord_id"
   end
 
@@ -52,14 +51,14 @@ ActiveRecord::Schema.define(version: 2020_12_13_041650) do
     t.string "klass"
     t.string "sub_klass"
     t.string "server"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "warcraft_logs_fights", force: :cascade do |t|
     t.bigint "warcraft_logs_reports_id"
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.datetime "start_time", precision: nil
+    t.datetime "end_time", precision: nil
     t.integer "boss"
     t.string "name"
     t.integer "size"
@@ -69,19 +68,19 @@ ActiveRecord::Schema.define(version: 2020_12_13_041650) do
     t.integer "bossPercentage"
     t.integer "fightPercentage"
     t.integer "lastPhaseForPercentageDisplay"
-    t.datetime "created_at", precision: 6, default: "2020-12-06 17:57:00", null: false
-    t.datetime "updated_at", precision: 6, default: "2020-12-06 17:57:00", null: false
+    t.datetime "created_at", default: "2020-12-06 17:57:00", null: false
+    t.datetime "updated_at", default: "2020-12-06 17:57:00", null: false
     t.index ["warcraft_logs_reports_id"], name: "index_warcraft_logs_fights_on_warcraft_logs_reports_id"
   end
 
   create_table "warcraft_logs_reports", id: :string, force: :cascade do |t|
     t.string "title"
     t.string "owner"
-    t.datetime "start"
-    t.datetime "end"
+    t.datetime "start", precision: nil
+    t.datetime "end", precision: nil
     t.string "zone"
-    t.datetime "created_at", precision: 6, default: "2020-12-06 17:57:00", null: false
-    t.datetime "updated_at", precision: 6, default: "2020-12-06 17:57:00", null: false
+    t.datetime "created_at", default: "2020-12-06 17:57:00", null: false
+    t.datetime "updated_at", default: "2020-12-06 17:57:00", null: false
     t.index ["id"], name: "index_warcraft_logs_reports_on_id"
     t.index ["owner"], name: "index_warcraft_logs_reports_on_owner"
   end
