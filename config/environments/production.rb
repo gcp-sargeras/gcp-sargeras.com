60edfec2d0a706355a2fd2a3a311499143aea6c0
Jacob Feldman <me@jacobfeldman.dev>
2022-12-12 23:34:45 -0500

    Turns on server static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Turn on server static files
+  config.serve_static_files = true
 end
