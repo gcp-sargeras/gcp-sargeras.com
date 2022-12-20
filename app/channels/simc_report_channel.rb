fce6c154e3d0bed4cbe33e0db658bcf3000f1c9e
Jacob Feldman <jacob.feldman@vergesense.com>
2022-12-19 20:19:48 -0500

    Removes unused channel

--- app/channels/simc_report_channel.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SimcReportChannel < ApplicationCable::Channel
-  def subscribe
-    report = Simc::Report.find(params[:id])
-
-    stream_for report
-  end
-end
